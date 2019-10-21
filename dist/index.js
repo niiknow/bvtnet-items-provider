558df7a4e55a68203204460ab66e1f76ed95b026
noogen <tom@noogen.net>
2019-10-21 17:03:37 -0500

    mocking it up

--- dist/index.js
+++ dist/index.js
@@ -153,17 +153,167 @@ function () {
       that.totalRows = 0;
       that.sortBy = '';
       that.sortDesc = false;
-      that.sortDirection = 'asc';
       that.filter = null;
-      that.filterOn = [];
+      that.filterIgnoredFields = [];
+      that.filterIncludedFields = [];
+      that.axiosConfig = {};
+      that.serverFields = {};
+      that.columns = [];
 
       if (that.fields) {
         var _fields = that.fields;
 
         for (var i = 0; i < _fields.length; i++) {
-          var field = _fields[i];
+          var _field = _fields[i];
+
+          if (!_field.key) {
+            _field = {
+              key: _field
+            };
+          }
+
+          _field.__index = i;
+          that.serverFields[_field.key] = _field;
+          that.columns.push(_field);
         }
       }
+    }
+    /**
+     * safely decode the string
+     * @param  String str
+     * @return String url decoded string
+     */
+
+  }, {
+    key: "decode",
+    value: function decode(str) {
+      try {
+        return decodeURIComponent(str.replace(/\+/g, ' '));
+      } catch (e) {
+        return str;
+      }
+    }
+    /**
+     * safely encode the string
+     * @param  String str
+     * @return String url encoded string
+     */
+
+  }, {
+    key: "encode",
+    value: function encode(str) {
+      try {
+        return encodeURIComponent(str);
+      } catch (e) {
+        return str;
+      }
+    }
+    /**
+     * helper method to parse querystring to object
+     * @param  String qstr the querystring
+     * @return Object      result
+     */
+
+  }, {
+    key: "queryParseString",
+    value: function queryParseString(qstr) {
+      qstr = (qstr || '').replace('?', '').replace('#', '');
+      var pattern = /(\w+)\[(\d+)\]/;
+      var decode = this.decode,
+          obj = {},
+          a = qstr.split('&');
+
+      for (var i = 0; i < a.length; i++) {
+        var parts = a[i].split('='),
+            key = decode(parts[0]),
+            m = pattern.exec(key);
+
+        if (m) {
+          obj[m[1]] = obj[m[1]] || [];
+          obj[m[1]][m[2]] = decode(parts[1]);
+          continue;
+        }
+
+        obj[parts[0]] = decode(parts[1] || '');
+      }
+
+      return obj;
+    }
+    /**
+     * reverse object to query string
+     * @param  Object obj the object
+     * @return String     the query string
+     */
+
+  }, {
+    key: "queryStringify",
+    value: function queryStringify(obj) {
+      var str = '',
+          encode = this.encode;
+
+      for (var k in obj) {
+        str += "&".concat(k, "=").concat(encode(obj[k]));
+      }
+
+      return str.replace('&', '');
+    }
+    /**
+     * translate the context to datatables.net query object
+     *
+     * @param  Object ctx the context object
+     * @return Object    the query object
+     */
+
+  }, {
+    key: "translateContext",
+    value: function translateContext(ctx) {
+      var that = this;
+      var query = {
+        draw: 1,
+        start: that.currentPage * that.perPage,
+        length: that.perPage,
+        search: {
+          value: "".concat(ctx.filter || ''),
+          regex: ctx.filter instanceof RegExp
+        },
+        order: [{
+          column: 0,
+          dir: ctx.sortDesc ? 'desc' : 'asc'
+        }],
+        columns: []
+      };
+
+      if (that.sortBy) {
+        query.order.column = (that.serverFields[that.sortBy] || {
+          __index: 0
+        }).__index;
+      }
+
+      for (var k in that.serverFields) {
+        field = that.serverFields[k];
+        var col = {
+          data: field.key,
+          name: field.key,
+          searchable: true,
+          orderable: field.sortable || true,
+          search: {
+            value: '',
+            regex: false
+          }
+        };
+
+        if (that.filterIgnoredFields && that.filterIgnoredFields.indexOf(field.key) > -1) {
+          col.searchable = false;
+        }
+
+        if (that.filterIncludedFields && that.filterIncludedFields.indexOf(field.key) > -1) {
+          col.searchable = true;
+        }
+
+        query.columns.push(col);
+      }
+
+      return query;
     }
     /**
     * the provider function to use with bootstrap vue
@@ -173,20 +323,38 @@ function () {
     */
 
   }, {
-    key: "provider",
-    value: function provider(ctx) {
+    key: "doQuery",
+    value: function doQuery(ctx) {
       var that = this;
+      var apiParts = ctx.apiUrl.split('?');
+      var query = {},
+          promise = null;
+
+      if (apiParts.length > 1) {
+        query = that.queryParseString(apiParts[1]);
+      }
+
+      query = that.translateContext(ctx, query);
       that.isBusy = true;
-      var promise = axios.get(ctx.apiUrl);
+      that.ajaxResponse = null;
+      that.ajaxError = null;
+      that.sortDirection = ctx.sortDesc ? 'desc' : 'asc';
+
+      if (that.method === 'POST') {
+        promise = that.axios.post(ctx.apiUrl, query, that.axiosConfig);
+      } else {
+        var apiUrl = apiParts[0] + '?' + that.queryStringify(query);
+        promise = that.axios.get(apiUrl, that.axiosConfig);
+      }
+
       return promise.then(function (rsp) {
-        that.response = rsp;
+        that.ajaxResponse = rsp;
         that.totalRows = rsp.recordsTotal;
-        var items = rsp.data;
         that.isBusy = false;
-        return items;
+        return rsp.data;
       }).catch(function (error) {
         that.isBusy = false;
-        that.error = error;
+        that.ajaxError = error;
         return [];
       });
     }
--- src/index.js
+++ src/index.js
@@ -25,17 +25,145 @@ class ItemsProviderWrapper {
     that.totalRows = 0
     that.sortBy = ''
     that.sortDesc = false
-    that.sortDirection = 'asc'
     that.filter = null
-    that.filterOn = []
+    that.filterIgnoredFields = []
+    that.filterIncludedFields = []
+    that.axiosConfig = {}
+    that.serverFields = {}
+    that.columns = []
 
     if (that.fields) {
       const fields = that.fields
 
       for (let i = 0; i < fields.length; i++) {
-        const field = fields[i]
+        let field = fields[i]
+        if (!field.key) {
+          field = { key: field }
+        }
+        field.__index = i
+
+        that.serverFields[field.key] = field
+
+        that.columns.push(field)
       }
     }
+  }
+
+  /**
+   * safely decode the string
+   * @param  String str
+   * @return String url decoded string
+   */
+  decode(str) {
+    try {
+      return decodeURIComponent(str.replace(/\+/g, ' '))
+    } catch (e) {
+      return str
+    }
+  }
+
+  /**
+   * safely encode the string
+   * @param  String str
+   * @return String url encoded string
+   */
+  encode(str) {
+    try {
+      return encodeURIComponent(str)
+    } catch (e) {
+      return str
+    }
+  }
+
+  /**
+   * helper method to parse querystring to object
+   * @param  String qstr the querystring
+   * @return Object      result
+   */
+  queryParseString(qstr) {
+    qstr = (qstr || '').replace('?', '').replace('#', '')
+
+    const pattern = /(\w+)\[(\d+)\]/
+    const decode = this.decode,
+      obj = {},
+      a = qstr.split('&')
+
+    for (let i = 0; i < a.length; i++) {
+      let parts = a[i].split('='),
+        key = decode(parts[0]),
+        m = pattern.exec(key)
+
+      if (m) {
+        obj[m[1]] = obj[m[1]] || []
+        obj[m[1]][m[2]] = decode(parts[1])
+        continue
+      }
+
+      obj[parts[0]] = decode(parts[1] || '')
+    }
+
+    return obj
+  }
+
+  /**
+   * reverse object to query string
+   * @param  Object obj the object
+   * @return String     the query string
+   */
+  queryStringify(obj) {
+    let str = '',
+      encode = this.encode
+
+    for(let k in obj) {
+      str += `&${k}=${encode(obj[k])}`
+    }
+
+    return str.replace('&', '')
+  }
+
+  /**
+   * translate the context to datatables.net query object
+   *
+   * @param  Object ctx the context object
+   * @return Object    the query object
+   */
+  translateContext(ctx) {
+    const that = this
+    const query = {
+      draw: 1,
+      start: that.currentPage * that.perPage,
+      length: that.perPage,
+      search: { value: `${ctx.filter || ''}`, regex: (ctx.filter instanceof RegExp) },
+      order:[ { column: 0, dir: ctx.sortDesc ? 'desc' : 'asc' } ],
+      columns: []
+    }
+
+    if (that.sortBy) {
+      query.order.column = (that.serverFields[that.sortBy] || { __index: 0 }).__index
+    }
+
+    for(let k in that.serverFields) {
+      field = that.serverFields[k]
+      const col = {
+        data: field.key,
+        name: field.key,
+        searchable: true,
+        orderable: field.sortable || true,
+        search: { value: '', regex: false }
+      }
+
+      if (that.filterIgnoredFields && that.filterIgnoredFields.indexOf(field.key) > -1) {
+        col.searchable = false
+      }
+
+      if (that.filterIncludedFields && that.filterIncludedFields.indexOf(field.key) > -1) {
+        col.searchable = true
+      }
+
+      query.columns.push(col)
+    }
+
+    return query
   }
 
   /**
@@ -44,23 +172,38 @@ class ItemsProviderWrapper {
 	 * @param  Object ctx bootstrap-vue context object
 	 * @return Array   array of items
 	 */
-  provider(ctx) {
+  doQuery(ctx) {
     const that = this
+    const apiParts = ctx.apiUrl.split('?')
+    let query = {},
+      promise = null
+
+    if (apiParts.length > 1) {
+      query = that.queryParseString(apiParts[1])
+    }
+
+    query = that.translateContext(ctx, query)
 
     that.isBusy = true
+    that.ajaxResponse = null
+    that.ajaxError = null
+    that.sortDirection = ctx.sortDesc ? 'desc' : 'asc'
 
-    const promise = axios.get(ctx.apiUrl)
+    if (that.method === 'POST') {
+      promise = that.axios.post(ctx.apiUrl, query, that.axiosConfig)
+    } else {
+      const apiUrl = apiParts[0] + '?' + that.queryStringify(query)
+      promise = that.axios.get(apiUrl, that.axiosConfig)
+    }
 
     return promise.then((rsp) => {
-      that.response = rsp
+      that.ajaxResponse = rsp
    		that.totalRows = rsp.recordsTotal
-      const items = rsp.data
-
       that.isBusy = false
-      return (items)
+      return (rsp.data)
     }).catch(error => {
       that.isBusy = false
-      that.error = error
+      that.ajaxError = error
       return []
     })
   }
